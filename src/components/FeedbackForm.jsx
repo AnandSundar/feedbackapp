3dfb4fae56dda614d5ecf56cafe1d58001c4ca2c
AnandSundar <anandsundar96@gmail.com>
2022-02-19 15:58:19 -0600

    added component Button.jsx

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
 
   const handleTextChange = (e) => {
+    //   console.log(e.target.value)
     setText(e.target.value)
   }
 
@@ -14,7 +16,7 @@ function FeedbackForm() {
             <h2>how would you rate your service with us ?</h2>
             <div className="input-group">
                 <input onChange={handleTextChange} type='text' placeholder='Write a review' value={text} />
-                <button type='submit'>Submit</button>
+                <Button type='submit'>Submit</Button>
             </div>
         </form>
     </Card>
