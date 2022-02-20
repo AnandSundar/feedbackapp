9df3721db412cda1866c17c5fd835670c16a47f6
AnandSundar <anandsundar96@gmail.com>
2022-02-19 21:53:00 -0600

    adjust FeedbackContext addFeedback to use db

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,6 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4} from 'uuid'
-
 
 const FeedbackContext = createContext()
 
@@ -20,7 +18,7 @@ export const FeedbackProvider = ({children}) => {
     //fetch feedback
 
     const fetchFeedback = async () => {
-        const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+        const response = await fetch('/feedback?_sort=id&_order=desc')
         const data = await response.json()
 
         setFeedback(data)
@@ -44,10 +42,18 @@ export const FeedbackProvider = ({children}) => {
         setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
     }
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        console.log(newFeedback)
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback),
+        })
+
+        const data = await response.json()
+        setFeedback([data, ...feedback])
     }
 
     return <FeedbackContext.Provider value={{
