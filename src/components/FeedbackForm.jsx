8b498c4dba35917d18c09d8d2d8b2241e15acbeb
AnandSundar <anandsundar96@gmail.com>
2022-02-19 20:47:48 -0600

    fixed some bug and cleaned up code

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import FeedbackContext from '../context/FeedbackContext'
 import Card from './shared/Card'
 import Button from './shared/Button'
@@ -6,7 +6,15 @@ import RatingSelect from './RatingSelect'
 
 function FeedbackForm() {
 
-  const {addFeedback} = useContext(FeedbackContext)
+  const {addFeedback, feedbackEdit, updateFeedback} = useContext(FeedbackContext)
+
+  useEffect(() => {
+    if(feedbackEdit.edit === true) {
+        setBtnDisabled(false)
+        setText(feedbackEdit.item.text)
+        setRating(feedbackEdit.item.rating)
+    }
+  },[feedbackEdit])
 
   const [text, setText] = useState('')
   const [rating, setRating] = useState(10)
@@ -37,10 +45,14 @@ function FeedbackForm() {
             rating: rating
         }
 
-        addFeedback(newFeedback)
+        if (feedbackEdit.edit === true) {
+            updateFeedback(feedbackEdit.item.id, newFeedback)
+        }else{
+            addFeedback(newFeedback)
+        }
+        setText('')
     }
 
-    setText('')
   }
 
   return (
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -17,8 +17,8 @@ export const FeedbackProvider = ({children}) => {
             rating: 9
         },
         {
-            id: 2,
-            text: 'this is feedback item 2',
+            id: 3,
+            text: 'this is feedback item 3',
             rating: 7
         }
     ])
@@ -42,6 +42,10 @@ export const FeedbackProvider = ({children}) => {
         })
     }
 
+    const updateFeedback = (id, updItem) => {
+        setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+    }
+
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
         console.log(newFeedback)
@@ -50,9 +54,12 @@ export const FeedbackProvider = ({children}) => {
 
     return <FeedbackContext.Provider value={{
         feedback: feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
+        updateFeedback,
+
     }}>
         {children}
     </FeedbackContext.Provider>
