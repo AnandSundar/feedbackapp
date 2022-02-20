61e77cef90fb2e1315aa438838af27f78f4a1314
AnandSundar <anandsundar96@gmail.com>
2022-02-19 21:42:07 -0600

    added spinner component to feedbackcontext and db.json

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,27 +1,20 @@
 import {motion, AnimatePresence} from 'framer-motion'
 import React, {useContext} from 'react'
 import Feedbackitem from './Feedbackitem'
+import Spinner from './shared/Spinner'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackList() {
 
-    const {feedback} = useContext(FeedbackContext)
+    const {feedback, isLoading} = useContext(FeedbackContext)
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback yet</p>
     }
-//   return (
-//     <>
-//         <div className="feedback-list">
-//             {feedback.map((item) => (
-//                 <Feedbackitem key={item.id} item={item} handleDelete={handleDelete}/>
-//             ))}
-//         </div>
-//     </>
-//   )
 
-return (
-    <>
+    return isLoading ? (
+        <Spinner />
+    ) :(
         <div className="feedback-list">
             <AnimatePresence>
                 {feedback.map((item) => (
@@ -31,6 +24,11 @@ return (
                 ))}
             </AnimatePresence>
         </div>
+    )
+
+return (
+    <>
+        
     </>
   )
 }
